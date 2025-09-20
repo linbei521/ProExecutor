55c59d4a9d9b6bed71a71d89dd724e603ef71acd
linbei521 <linb38387@gmail.com>
2025-09-20 16:01:44 +0800

    创建 configs/mobile.lua

---
+++ configs/mobile.lua
@@ -0,0 +1,25 @@
+--[[
+    移动端配置
+]]
+
+return {
+    windowSize = {340, 400},
+    touchOptimized = true,
+    device = "mobile",
+    buttonSize = {80, 40},
+    fontSize = {
+        title = 14,
+        normal = 12,
+        small = 10
+    },
+    spacing = {
+        padding = 10,
+        margin = 5
+    },
+    features = {
+        autoComplete = false,
+        syntaxHighlight = false,
+        lineNumbers = false,
+        keyboardShortcuts = false
+    }
+}
