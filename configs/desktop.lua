6d18495a91bf498be20db3b8abe3d5d7bb73df5e
linbei521 <linb38387@gmail.com>
2025-09-20 16:02:06 +0800

    创建 desktop.lua

---
+++ configs/desktop.lua
@@ -0,0 +1,25 @@
+--[[
+    桌面端配置
+]]
+
+return {
+    windowSize = {450, 320},
+    touchOptimized = false,
+    device = "desktop",
+    buttonSize = {60, 30},
+    fontSize = {
+        title = 13,
+        normal = 11,
+        small = 9
+    },
+    spacing = {
+        padding = 8,
+        margin = 4
+    },
+    features = {
+        autoComplete = true,
+        syntaxHighlight = true,
+        lineNumbers = true,
+        keyboardShortcuts = true
+    }
+}
