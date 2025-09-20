fa2a1639be696603ccfb2dddfee340e0142cdfe1
linbei521 <linb38387@gmail.com>
2025-09-20 15:58:44 +0800

    创建 version.lua

---
+++ version.lua
@@ -0,0 +1,26 @@
+--[[
+    版本信息文件
+]]
+
+return {
+    version = "1.0.0",
+    minClientVersion = "1.0.0",
+    updateRequired = false,
+    updateMessage = "欢迎使用ProExecutor GitHub版！",
+    features = {
+        "模块化架构",
+        "移动端优化", 
+        "在线更新",
+        "脚本管理",
+        "语法高亮",
+        "导入导出"
+    },
+    changelog = {
+        ["1.0.0"] = {
+            "首次发布GitHub版",
+            "模块化重构",
+            "移动端适配",
+            "在线加载系统"
+        }
+    }
+}
